de183f6d2a89d2b4e5cd2c539ecd4881f832ea16
MauntbattenPink <kaltregen@gmail.com>
2016-08-07 20:55:10 +0300

    fixed gap for SOAP service serialization

--- src/com/random/task/services/JsonService.java
+++ src/com/random/task/services/JsonService.java
@@ -8,7 +8,7 @@ import java.io.IOException;
  * Created by olsh on 8/5/2016.
  */
 public class JsonService implements Service {
-    private static final JsonService INSTANCE = new JsonService();
+    private transient static final JsonService INSTANCE = new JsonService();
 
     private JsonService() {}
 
